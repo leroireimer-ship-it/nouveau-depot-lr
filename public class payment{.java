dfabc5fca9872d62ad3969dc237649327966d681
leroireimer-ship-it <leroireimer@gmail.com>
2025-12-04 11:32:17 +0100

    teste

---
+++ public class payment{.java
@@ -0,0 +1,7 @@
+public class payment{
+public static void main(string[]args){
+int j=31,s=25000,m;
+m=j*s;
+system.out.printin(¨le salaire journalier est¨+s+¨le nombre de jour dans un mois est¨+j+¨le salaire mensuel est¨+m);
+}
+}
