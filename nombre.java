7544798accb799564b176c8574e74318f19c7327
leroireimer-ship-it <leroireimer@gmail.com>
2025-12-04 18:40:04 +0100

    nombre

---
+++ nombre.java
@@ -0,0 +1,14 @@
+
+import java.util.scanner;
+public class division{
+public static void main(string[]args){
+scanner scanner = new scanner (system.in) ;
+system.out.print(¨entrez le premier nombre¨);
+int a = scanner.nextint();
+system.out.print(¨entrez le deuxieme nombre¨);
+int b = scanner.nextint();
+double d=a/b;
+system.out.print(¨la somme des trois nombres est¨+d);
+scanner.close;
+}
+}
